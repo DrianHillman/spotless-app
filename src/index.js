2a74a94ce0058357b663fd869808f965cac20a4b
Brian Douglas <bdougie@users.noreply.github.com>
2017-06-23 20:26:01 -0700

    delete duplicate routes to make the page render (#1)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { makeMainRoutes } from './routes';
+
+const routes = makeMainRoutes();
+
 ReactDOM.render(
-  <App />,
+  routes,
   document.getElementById('root')
 );
